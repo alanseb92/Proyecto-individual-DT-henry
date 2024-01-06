c016c8859b7563a437ebe2e50dd0d7c8dbf1ae1f
Alan Sebastián García <104204055+alanseb92@users.noreply.github.com>
2024-01-06 03:26:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,44 @@ import numpy as np
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Bienvenido a la API de Alan Sebastian Garcia"}
+@app.get("/", response_class=HTMLResponse)
+async def incio ():
+    principal= """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>API Steam</title>
+            <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    padding: 20px;
+                }
+                h1 {
+                    color: #333;
+                    text-align: center;
+                }
+                p {
+                    color: #666;
+                    text-align: center;
+                    font-size: 18px;
+                    margin-top: 20px;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>API de consultas sobre juegos de la plataforma Steam</h1>
+            <p>Bienvenido a la API de Steam donde se pueden hacer diferentes consultas sobre la plataforma de videojuegos.</p>
+            <p>INSTRUCCIONES:</p>
+            <p>Escriba <span style="background-color: lightgray;">/docs</span> a continuación de la URL actual de esta página para interactuar con la API</p>
+            <p>Consulte en el siguiente enlace:<a href="https://proyecto-individual-dt-henry.onrender.com/docs/">{{FastAPI}}</a></p>
+            <p> El desarrollo de este proyecto esta en <a href="https://github.com/alanseb92/Proyecto-individual-DT-henry"><img alt="GitHub" src="https://img.shields.io/badge/GitHub-black?style=flat-square&logo=github"></a></p>
+            <p>Alan Sebastián García - 2024 -</p>
+        </body>
+    </html>
+
+        """    
+    return principal
+
 
 #---------------------------------------------------------------------------------------------------------------------------
 
